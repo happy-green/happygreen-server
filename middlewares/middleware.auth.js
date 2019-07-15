c392b214188f6cc4386d7d7363afe45552d05dac
Abhi10699 <patelabhi10699@gmail.com>
2019-07-15 21:38:53 +0530

    middleware for route protection

---
+++ middlewares/middleware.auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const UserModel = require('../models/User');
+
+// protects routes..
+
+const checkToken = (req,res,next) => {
+  const token = req.header("x-auth-token");
+  if(!token) return res.status(400).json({msg:"Nahh.... we need a token bro"});
+  try {
+    const decoded = jwt.verify(token,"mySecret");    
+    req.userID = decoded.id;
+    return next();
+  } 
+  catch (e) {
+    console.log(e);
+    return res.send(400).json({msg:"Invalid shit"})
+  }
+}
+
+
+module.exports = checkToken;
