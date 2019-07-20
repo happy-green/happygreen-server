992cdcec1974185fbf679b15a4bdca1e63619f96
Abhi10699 <patelabhi10699@gmail.com>
2019-07-20 11:43:02 +0530

    added file upload endpoint

--- controllers/core.endpoints.js
+++ controllers/core.endpoints.js
@@ -1,7 +1,9 @@
 const authRoutes = require('./endpoints.auth');
 const userRoutes = require('./endpoints.user');
+const uploadRoutes = require('./endpoints.uploads');
 
 module.exports = (app)=>{
   app.use('/auth/',authRoutes);
-  app.use('/user/',userRoutes)
+  app.use('/user/',userRoutes);
+  app.use('/upload/',uploadRoutes);
 }
